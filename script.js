be25d544d542ed11aa63238a8dd76889607fe02e
GeorgeStep-mywork <71914792+GeorgeStep-mywork@users.noreply.github.com>
2020-10-02 15:33:42 +0530

    This is a Percentage Calculator
    This is a Percentage Calulator. Hope you will find it useful. Pls do Give a feedback Good or Bad in the end of the Webpage

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('container').onchange = function () {
+  var Phy = Number(document.getElementById('Phym').value);
+  var Che = Number(document.getElementById('Chem').value);
+  var Bio = Number(document.getElementById('Biom').value);
+  var Mat = Number(document.getElementById('Mathm').value);
+  var His = Number(document.getElementById('Hism').value);
+  var Geo = Number(document.getElementById('Geom').value);
+  var Eng = Number(document.getElementById('Engm').value);
+  var Sl = Number(document.getElementById('Slm').value);
+  var Com = Number(document.getElementById('Comm').value);
+  var tp = (Phy + Che + Bio + Mat + His + Geo + Eng + Sl + Com)*2 /9 ;
+  document.getElementById('tipOutput').innerHTML = tp + " %";
+}
+
+function SubForm (){
+    $.ajax({
+        url:'https://api.apispreadsheets.com/data/1700/',
+        type:'post',
+        data:$("#myForm").serializeArray(),
+        success: function(){
+          alert("Form Data Submitted :)")
+        },
+        error: function(){
+          alert("There was an error :(")
+        }
+    });
+}
